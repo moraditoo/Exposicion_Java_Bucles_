9fc1e32a3039ee59763384505171a75249d7034b
Mauro Delgado <maurods@uamv.edu.ni>
2026-03-20 15:25:59 -0600

    Se agrego el flujo por defecto de la aplicacion
    aaaa

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,9 +1,25 @@
-package main;
+package Main;
 
-public class Main
-{
-    public static void main(String[] args)
-    {
+import java.util.Scanner;
 
+public class Main {
+    public static void main(String[] args) {
+        while (true){
+            System.out.println("----- BIENVENIDO -----");
+            System.out.println("1. Retiro sin tarjeta");
+            System.out.println("2. Transaccion");
+            System.out.println("3. Cambiar pin");
+            System.out.println("4. Retiro");
+            Scanner sc = new Scanner(System.in);
+
+            int num = sc.nextInt();
+
+            if (num < 1 || num > 4){
+                System.out.println("Ingrese un numero valido");
+                continue;
+            }
+
+
+        }
     }
 }
